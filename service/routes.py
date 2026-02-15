a6866eb84fb10ffa07b009a89a0e71ec76cbba44
rrosfel <ramrosfel@gmail.com>
2026-02-15 08:06:12 -0500

    correcting linting error in routes.py

--- service/routes.py
+++ service/routes.py
@@ -89,6 +89,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     
+    
     return account.serialize(), status.HTTP_200_OK
 
 
